7379b15ee8547c8b8ea91cb220f6f8c541eb0ff3
Shivam Singh <shivamsinghgbpit@gmail.com>
2020-09-20 10:01:48 +0530

    change index

--- index.js
+++ index.js
@@ -25,15 +25,13 @@ const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
 chatServer.listen(5000);
 console.log('chat server is listening on port 5000');
 
-if (env.name == 'development') {
-    app.use(sassMiddleware({
-        src: path.join(__dirname, env.asset_path, 'scss'),
-        dest: path.join(__dirname, env.asset_path, 'css'),
-        debug: true,
-        outputStyle: 'extended',
-        prefix: '/css'
-    }));
-}
+app.use(sassMiddleware({
+    src: path.join(__dirname, env.asset_path, 'scss'),
+    dest: path.join(__dirname, env.asset_path, 'css'),
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css'
+}));
 
 app.use(express.urlencoded());
 
